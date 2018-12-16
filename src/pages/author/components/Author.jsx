c501fb465a41823aaeb09fedf40e8d965cbc9db0
NgeKaworu <NgeKaworu@163.com>
2018-12-16 20:58:24 +0800

    多item

---
+++ src/pages/author/components/Author.jsx
@@ -0,0 +1,16 @@
+import { connect } from "dva";
+import React from "react";
+import WordCloud from '../../../components/WordCloud'
+
+@connect(state => ({
+  author: state.author.list
+}))
+class Author extends React.Component {
+  render() {
+    const { author } = this.props
+    const data = author.map(d => ({ text: d.author, size: d.count }))
+    return <WordCloud data={data}/>
+  }
+}
+
+export default Author;
--- src/pages/author/page.js
+++ src/pages/author/page.js
@@ -1,16 +1,3 @@
-import { connect } from "dva";
-import React from "react";
-import WordCloud from '../../components/WordCloud'
+import Author from './components/Author'
 
-@connect(state => ({
-  author: state.author.list
-}))
-class Author extends React.Component {
-  render() {
-    const { author } = this.props
-    const data = author.map(d => ({ text: d.author, size: d.count }))
-    return <WordCloud data={data}/>
-  }
-}
-
-export default Author;
+export default () => <Author />
