ebf54b096259d6548881a848e787dfd8b31a9302
NgeKaworu <NgeKaworu@163.com>
2018-12-30 21:55:09 +0800

    升级umi2 改用hash路由, 重新打包部署

--- .umirc.js
+++ .umirc.js
@@ -1,10 +1,25 @@
 export default {
-  plugins: ['umi-plugin-dva'],
+  plugins: [
+    [
+      'umi-plugin-react',
+      {
+        dva: true,
+        antd: true,
+        dynamicImport: {
+          webpackChunkName: true,
+        },
+      }
+    ],
+  ],
   proxy: {
     '/api': {
       'target': "http://api.furan.xyz/",
       "changeOrigin": true,
       "pathRewrite": { "^/api": "" }
     }
-  }
+  },
+  base: '/d3/',
+  publicPath: '/d3/',
+  exportStatic: true,
+  history: 'hash',
 }
