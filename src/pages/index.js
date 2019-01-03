98328734d78ff9d1452004974871d0e91e831b03
NgeKaworu <NgeKaworu@163.com>
2019-01-03 14:14:06 +0800

    修改主页

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { connect } from 'dva';
-import styles from './index.css';
+import React from "react";
+import { connect } from "dva";
+import styles from "./index.css";
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
       <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva">Getting Started</a></li>
-      </ul>
+      <h1>这是React和D3实现的词云demo</h1>
+      <p>
+        请点击上方的导航条切换到详细页测试
+      </p>
+      <p>
+        技术栈
+        <a target="_blank" href="https://react.docschina.org/docs/hello-world.html">react</a>+
+        <a target="_blank" href="https://ant.design/docs/react/introduce-cn">
+          antd
+        </a>
+         + <a target="_blank" href="https://dvajs.com/guide/">dva</a> +
+        <a target="_blank" href="https://umijs.org/zh/guide/">umi</a> +
+        <a target="_blank" href="https://github.com/d3/d3/wikii">d3</a> +
+        <a target="_blank" href="https://github.com/jasondavies/d3-cloud">d3词云插件</a>
+      </p>
+      <p>
+        <a target="_blank" href="https://github.com/NgeKaworu/auto-depoly-test">仓库地址</a>
+      </p>
+      <p>谢谢浏览ˋ( ° ▽、° )</p>
     </div>
   );
 }
 
-IndexPage.propTypes = {
-};
+IndexPage.propTypes = {};
 
 export default connect()(IndexPage);
